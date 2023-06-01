714d5733dff83ea89d62d7ef5cb871b0e7b2ffdc
MaryamAmanat <113723249+MaryamAmanat@users.noreply.github.com>
2023-06-01 20:12:24 +0500

    Update c.py

--- c.py
+++ c.py
@@ -109,7 +109,7 @@ def main():
     if page == "Login":
         login()
          if login():  # Check if login is successful
-            career_counseling_app()
+           career_counseling_app()
     elif page == "Signup":
         signup()
     elif page == "Career Counseling":
