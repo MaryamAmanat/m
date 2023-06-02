ce42b7ec3dcab66eba940acdaa7fd8bfb67de841
MaryamAmanat <113723249+MaryamAmanat@users.noreply.github.com>
2023-06-02 19:13:47 +0500

    Update g.py

--- g.py
+++ g.py
@@ -51,25 +51,27 @@ def display_report():
     csv_files = [file for file in os.listdir("data") if file.endswith(".csv")]
 
     if csv_files:
-        # Combine all the CSV data into a single DataFrame
-        dfs = []
+        # Select date for filtering
+        selected_date = st.date_input("Select Date")
+        
+        # Filter records for the selected date
+        filtered_data = []
         for file in csv_files:
             df = pd.read_csv(os.path.join("data", file))
-            dfs.append(df)
-        if len(dfs) > 0:
-            combined_df = pd.concat(dfs, ignore_index=True)
+            df['Date'] = pd.to_datetime(df['Date'])
+            filtered_df = df[df['Date'] == selected_date]
+            if not filtered_df.empty:
+                filtered_data.append(filtered_df)
+        
+        if filtered_data:
+            # Combine all the filtered DataFrames into a single DataFrame
+            combined_df = pd.concat(filtered_data, ignore_index=True)
             st.dataframe(combined_df)
         else:
-            st.warning("No data available.")
+            st.warning("No data available for the selected date.")
     else:
         st.warning("No data files found.")
 
-    # Clear the report
-    if st.button("Clear Report"):
-        # Delete all the data files
-        for file in csv_files:
-            os.remove(os.path.join("data", file))
-        st.warning("Report cleared successfully!")
 
 
 def main():
