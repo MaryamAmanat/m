9ad90ad848cefad3b24425c4c5bb5549e243c7f5
MaryamAmanat <113723249+MaryamAmanat@users.noreply.github.com>
2023-06-02 19:08:23 +0500

    Update g.py

--- g.py
+++ g.py
@@ -24,25 +24,25 @@ def data_entry():
             st.success(f"New file created: {filename}")
 
     # Take income details
-    while st.button("Add Income"):
-        income_type = st.radio("Income Type", ["Salary", "Blog Income", "Other Income"])
-        income_amount = st.number_input("Income Amount (USD)", min_value=0.0, step=0.01)
-
-        # Append income data to the file
-        with open(filename, "a") as f:
-            f.write(f"{date},{income_type},{income_amount},,,\n")
-        st.success("Income entry added successfully!")
+    st.subheader("Income Details")
+    income_type = st.radio("Income Type", ["Salary", "Blog Income", "Other Income"])
+    income_amount = st.number_input("Income Amount (USD)", min_value=0.0, step=0.01)
 
     # Take expense details
-    while st.button("Add Expense"):
-        expense_type = st.radio("Expense Type", ["Rent", "Car Expense", "Grocery Expense", "Other Expense"])
-        expense_amount = st.number_input("Expense Amount (USD)", min_value=0.0, step=0.01)
-        comments = st.text_area("Comments")
+    st.subheader("Expense Details")
+    expense_type = st.radio("Expense Type", ["Rent", "Car Expense", "Grocery Expense", "Other Expense"])
+    expense_amount = st.number_input("Expense Amount (USD)", min_value=0.0, step=0.01)
 
-        # Append expense data to the file
-        with open(filename, "a") as f:
-            f.write(f"{date},,{income_type},{expense_type},{expense_amount},{comments}\n")
-        st.success("Expense entry added successfully!")
+    comments = st.text_area("Comments")
+
+    # Append data to the file
+    with open(filename, "a") as f:
+        f.write(f"{date},{income_type},{income_amount},{expense_type},{expense_amount},{comments}\n")
+    st.success("Data entry added successfully!")
+
+    # Save the data for the current date
+    if st.button("Save"):
+        st.success("Data saved successfully!")
 
 def display_report():
     st.subheader("Data Report")
@@ -58,25 +58,15 @@ def display_report():
             dfs.append(df)
         if len(dfs) > 0:
             combined_df = pd.concat(dfs, ignore_index=True)
-
-            # Group the data by month and calculate totals
-            grouped_df = combined_df.groupby(combined_df['Date'].dt.strftime('%B'))[['Income Amount', 'Expense Amount']].sum().reset_index()
-            grouped_df = grouped_df.rename(columns={'Date': 'Month'})
-            st.dataframe(grouped_df)
-
-            # Calculate net income
-            net_income = grouped_df['Income Amount'].sum() - grouped_df['Expense Amount'].sum()
-            st.subheader("Net Income")
-            st.write(f"${net_income:.2f}")
-
-            # Display detailed report
-            st.subheader("Detailed Report")
             st.dataframe(combined_df)
-
         else:
             st.warning("No data available.")
     else:
         st.warning("No data files found.")
+
+    # Clear the report
+    if st.button("Clear Report"):
+        st.warning("Report cleared successfully!")
 
 def main():
     st.title("Expense and Income Tracker")
@@ -89,5 +79,5 @@ def main():
     elif option == "Display Report":
         display_report()
 
-if __name__ == "__main__":
+if __name__ == '__main__':
     main()
